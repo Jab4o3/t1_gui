9c846b055529117182dbe98cf39430e43eef9b16
Vladislav Serafimov <vladiserafimov@abv.bg>
2026-02-12 15:20:24 +0200

    Basic T1 sequence generation done

--- main_gui.py
+++ main_gui.py
@@ -1,9 +1,20 @@
+from datetime import datetime
 import tkinter as tk
 from tkinter import ttk
 from matplotlib.backend_bases import key_press_handler
 from matplotlib.backends.backend_tkagg import (FigureCanvasTkAgg, NavigationToolbar2Tk)
 from matplotlib.figure import Figure
 import numpy as np
+from dwfconstants import *
+import sys
+from math import log10
+
+if sys.platform.startswith("win"):
+    dwf = cdll.dwf
+elif sys.platform.startswith("darwin"):
+    dwf = cdll.LoadLibrary("/Library/Frameworks/dwf.framework/dwf")
+else:
+    dwf = cdll.LoadLibrary("libdwf.so")
 
 """
 Main GUI class
@@ -12,10 +23,13 @@ Main GUI class
 
 class Gui:
     def __init__(self, root, title="T1 measurements"):
+        # =GUI=
         self.root = root
         self.root.title(title)
         self.root.resizable(height=False, width=False)  # make window non-resizable
         # associate entries and scales with object for callback references
+        # device status entry
+        self.status = tk.StringVar(root, value="Not connected")
         # minimum dark time
         self.td_min = tk.DoubleVar(root, value=1)
         self.scale_td_min = None
@@ -28,17 +42,116 @@ class Gui:
         self.dps = tk.DoubleVar(root, value=10)
         self.scale_dps = None
         self.entry_dps = None
+        # tree view for running log and status messages
+        self.tree_running_log = None
 
+        # startup code for creating and rendering app
         self.populate()
+
+        # =AD2=
+        self.hdwf = c_int()  # AD2 handle
+        self.laser_channel = c_int(0)  # wavegen channel 1
+        self.lia_channel = c_int(1)  # wavegen channel 2
+        self.find_device()  # look for an AD2 on startup
+
+        # =T1 measurements=
+        self.sequences = []
+
+    """
+    Generate a pseudo-logarithmic integer space for generating dark times with different sample sizes
+    """
+
+    def gen_log_space(self, td_min, td_max, n):
+        result = [td_min]
+        # return 1 if bad n
+        if n <= 1:
+            return result
+
+        for i in range(n - 1):
+            td_curr = round(10 ** (i * log10(td_max) / (n - 1)))
+            # check if dark time is repeating last value and increment it if yes
+            if td_curr - result[-1] >= 1:
+                result.append(td_curr + td_min)
+            else:
+                result.append(result[-1] + 1 + td_min)
+        result.append(td_max)  # add last value manually to eliminate offset
+        return result
+
+
+    """
+    Connect to AD2 or show that it is not connected
+    """
+
+    def find_device(self):
+        # TODO: make a button associated with the find function
+        # TODO: add disconnect handler
+        # version required for operation
+        version = create_string_buffer(16)
+        dwf.FDwfGetVersion(version)
+        print("DWF Version: " + str(version.value))
+
+        # define behavior on close (0 = run, 1 = stop, 2 = shutdown)
+        dwf.FDwfParamSet(DwfParamOnClose, c_int(0))
+
+        # open device and get interface reference
+        if dwf.FDwfDeviceOpen(c_int(-1), byref(self.hdwf)):
+            self.log_message("AD2", "Connection", f"Connected to AD2, DWF Version {str(version.value)}")
+            self.status.set("Connected")
+        else:
+            szerr = create_string_buffer(512)
+            dwf.FDwfGetLastErrorMsg(szerr)
+            self.log_message("AD2", "Connection", szerr.value)
+
+        # disable auto config for better performance
+        dwf.FDwfDeviceAutoConfigureSet(self.hdwf, c_int(0))
+
+    """
+    Generate sequences for pulsing
+    """
+
+    def t1_generate_sequences(self):
+        # TODO: add channel syncing and LIA sequences
+        # constants
+        pattern_size = 4096  # fixed pattern buffer size
+        f_pattern = 1 / (pattern_size * self.td_min.get())  # set frequency of the whole pattern
+        # fixed minimum dark time to pulse width ratio
+        min_td = 1  # dark time (in buffer slots)
+        max_td = int(self.td_max.get() * f_pattern)  # TODO: verify formula
+        pw = 5  # pulse width (in buffer slots)
+        all_tds = self.gen_log_space(td_min=min_td, td_max=max_td, n=self.dps.get()) # pseudo-logspace of dark times
+
+        # make pattern
+        td = min_td
+        # loop for all data points
+        for curr_td in all_tds:
+            pattern = (c_double * pattern_size)(0)  # initialize C-style zero array
+            for i in range(0, pw):
+                pattern[i] = 1  # cancellation pulse
+                pattern[int(i + (pattern_size / 2))] = 1  # initialization pulse
+                pattern[int(i + pw + td + (pattern_size / 2))] = 1  # readout pulse
+
 
     """
     Set value of Tkinter entry
     """
 
     def update_entry(self, e, value):
+        if e is None:
+            raise NameError(f"Entry is not initialized yet, value {value} not updated")
         e.delete(0, tk.END)
         e.insert(0, value)
         return
+
+    """
+    Logger function for GUI log
+    """
+
+    def log_message(self, source, status, value):
+        if self.tree_running_log is None:
+            raise NameError("Tree is not initialized yet")
+        else:
+            timestamp = datetime.now().strftime("%H:%M:%S")
+            self.tree_running_log.insert('', tk.END, text=timestamp, values=(source, status, value))
 
     """
     Callback for updating minimum dark time variable and entry based on slider
@@ -102,20 +215,56 @@ class Gui:
     def populate(self):
         # create frames
         lf_devices = ttk.Labelframe(self.root, text="Devices")
+        lf_status = ttk.Labelframe(self.root, text="Test status")
         lf_inputs = ttk.Labelframe(self.root, text="T1 parameters")
         lf_data = ttk.Labelframe(self.root, text="Test data")
 
         # put frames in app
         lf_devices.grid(row=0, column=0, padx=5, pady=5, sticky=("N", "W", "E", "S"))
-        lf_inputs.grid(row=1, column=0, padx=5, pady=5, sticky=("N", "W", "E", "S"))
-        lf_data.grid(row=0, column=1, rowspan=2, padx=5, pady=5, sticky=("N", "W", "E", "S"))
+        lf_status.grid(row=1, column=0, padx=5, pady=5, sticky=("N", "W", "E", "S"))
+        lf_inputs.grid(row=2, column=0, padx=5, pady=5, sticky=("N", "W", "E", "S"))
+        lf_data.grid(row=0, column=1, rowspan=3, padx=5, pady=5, sticky=("N", "W", "E", "S"))
 
         # =DEVICE FRAME=
         # create labels
-        label_dev = ttk.Label(lf_devices, text="Device")
+        label_name = ttk.Label(lf_devices, text="Device name")
+        label_status = ttk.Label(lf_devices, text="Device status")
 
         # put labels in app
-        label_dev.grid(row=0, column=0, sticky="W", padx=5, pady=5)
+        label_name.grid(row=0, column=0, sticky="W", padx=5, pady=5)
+        label_status.grid(row=1, column=0, sticky="W", padx=5, pady=5)
+
+        # create entries
+        entry_name = ttk.Entry(lf_devices)
+        self.update_entry(entry_name, "Analog Discovery 2")
+        entry_name.config(state="readonly")
+        entry_status = ttk.Entry(lf_devices, state="readonly", textvariable=self.status)
+
+        # put entries in app
+        entry_name.grid(row=0, column=1, sticky="W", padx=5, pady=5)
+        entry_status.grid(row=1, column=1, sticky="W", padx=5, pady=5)
+
+        # =STATUS FRAME=
+        # create tree
+        self.tree_running_log = ttk.Treeview(lf_status, columns=("Source", "Status", "Value"))
+
+        # label columns
+        self.tree_running_log.heading("#0", text="Time")
+        self.tree_running_log.heading("Source", text="Source")
+        self.tree_running_log.heading("Status", text="Status")
+        self.tree_running_log.heading("Value", text="Value")
+
+        # set column size
+        self.tree_running_log.column("#0", minwidth=10, width=70)
+        self.tree_running_log.column("Source", minwidth=10, width=70)
+        self.tree_running_log.column("Status", minwidth=10, width=70)
+        self.tree_running_log.column("Value", minwidth=10, width=210)
+
+        # put tree in app
+        self.tree_running_log.grid(row=0, column=0, sticky=("N", "W", "E", "S"), padx=5, pady=5)
+
+        # log first status message
+        self.log_message("GUI", "Startup", "GUI created")
 
         # =INPUT FRAME=
         # create labels
@@ -133,21 +282,30 @@ class Gui:
         self.entry_td_max = ttk.Entry(lf_inputs)
         self.entry_dps = ttk.Entry(lf_inputs)
 
-        # create scalers and bind to callbacks
+        # create sliders and bind to callbacks
         self.scale_td_min = tk.Scale(lf_inputs, from_=1, to=100, orient=tk.HORIZONTAL)
         self.scale_td_min.bind("<ButtonRelease-1>", self.callback_s_td_min)
+        self.scale_td_min.set(self.td_min.get())
         self.scale_td_max = tk.Scale(lf_inputs, from_=1, to=100, orient=tk.HORIZONTAL)
         self.scale_td_max.bind("<ButtonRelease-1>", self.callback_s_td_max)
+        self.scale_td_max.set(self.td_max.get())
         self.scale_dps = tk.Scale(lf_inputs, from_=1, to=100, orient=tk.HORIZONTAL)
         self.scale_dps.bind("<ButtonRelease-1>", self.callback_s_dps)
+        self.scale_dps.set(self.dps.get())
 
-        # put entry boxes in app and initialize their values to the default ones
+        # put entry boxes in app, bind them to callbacks and initialize their values to the default ones
         self.entry_td_min.grid(row=0, column=2, padx=5, pady=5)
         self.entry_td_min.bind("<Return>", self.callback_e_td_min)
+        self.entry_td_min.bind("<FocusOut>", self.callback_e_td_min)
+        self.update_entry(self.entry_td_min, self.td_min.get())
         self.entry_td_max.grid(row=1, column=2, padx=5, pady=5)
         self.entry_td_max.bind("<Return>", self.callback_e_td_max)
+        self.entry_td_max.bind("<FocusOut>", self.callback_e_td_max)
+        self.update_entry(self.entry_td_max, self.td_max.get())
         self.entry_dps.grid(row=2, column=2, padx=5, pady=5)
         self.entry_dps.bind("<Return>", self.callback_e_dps)
+        self.entry_dps.bind("<FocusOut>", self.callback_e_dps)
+        self.update_entry(self.entry_dps, self.dps.get())
 
         # put scalers in app
         self.scale_td_min.grid(row=0, column=1, padx=5, pady=5)
@@ -168,6 +326,7 @@ class Gui:
         toolbar = NavigationToolbar2Tk(canvas, lf_data, pack_toolbar=False)
         toolbar.update()
 
+        # put figure on canvas
         canvas.get_tk_widget().grid(row=0, column=0, padx=5, pady=5)
         toolbar.grid(row=1, column=0, padx=5, pady=5)
 
--- pulse_gen.py
+++ pulse_gen.py
@@ -13,13 +13,13 @@ elif sys.platform.startswith("darwin"):
 else:
     dwf = cdll.LoadLibrary("libdwf.so")
 
-hzFreq = 100
+f_pattern = 100
 pattern_size = 4096
 td = 1  # dark time (in buffer slots)
 pw = 5  # pulse width (in buffer slots)
 print("+++++++++++++++++++++++++++")
-print("Dark time: " + str((10 ** 6) * td / (hzFreq * pattern_size)) + " us")
-print("Pulse width: " + str((10 ** 6) * pw / (hzFreq * pattern_size)) + " us")
+print("Dark time: " + str((10 ** 6) * td / (f_pattern * pattern_size)) + " us")
+print("Pulse width: " + str((10 ** 6) * pw / (f_pattern * pattern_size)) + " us")
 print("+++++++++++++++++++++++++++")
 hdwf = c_int()
 # pattern = (c_double * pattern_size)()
@@ -62,9 +62,9 @@ if hdwf.value == hdwfNone.value:
 # disable auto config for better performance
 dwf.FDwfDeviceAutoConfigureSet(hdwf, c_int(0))
 
-phzFreq = c_double()
-dwf.FDwfDigitalOutInternalClockInfo(hdwf, byref(phzFreq))
-print("Clock frequency: " + phzFreq.value.__str__())
+pf_pattern = c_double()
+dwf.FDwfDigitalOutInternalClockInfo(hdwf, byref(pf_pattern))
+print("Clock frequency: " + pf_pattern.value.__str__())
 
 print("Generating custom waveform...")
 print("Enable: " + dwf.FDwfAnalogOutNodeEnableSet(hdwf, channel, AnalogOutNodeCarrier, c_int(1)).__str__())
@@ -73,13 +73,13 @@ print("Set function to custom: " + dwf.FDwfAnalogOutNodeFunctionSet(hdwf, channe
 print("Set data pattern: " + dwf.FDwfAnalogOutNodeDataSet(hdwf, channel, AnalogOutNodeCarrier, pattern,
                                                           c_int(pattern_size)).__str__())
 print("Set frequency: " + dwf.FDwfAnalogOutNodeFrequencySet(hdwf, channel, AnalogOutNodeCarrier,
-                                                            c_double(hzFreq)).__str__())
+                                                            c_double(f_pattern)).__str__())
 print(
     "Set amplitude: " + dwf.FDwfAnalogOutNodeAmplitudeSet(hdwf, channel, AnalogOutNodeCarrier, c_double(5.0)).__str__())
 
 print("Run period set: " + dwf.FDwfAnalogOutRunSet(hdwf, channel,
-                                                   c_double(10000.0 / hzFreq)).__str__())  # run for 2 periods
-# print("Wait period set: " + dwf.FDwfAnalogOutWaitSet(hdwf, channel, c_double(1000.0 / hzFreq)).__str__())  # wait one pulse time
+                                                   c_double(10000.0 / f_pattern)).__str__())  # run for 2 periods
+# print("Wait period set: " + dwf.FDwfAnalogOutWaitSet(hdwf, channel, c_double(1000.0 / f_pattern)).__str__())  # wait one pulse time
 print("Repetitions set: " + dwf.FDwfAnalogOutRepeatSet(hdwf, channel, c_int(3)).__str__())  # repeat 3 times
 
 print("Output config: " + dwf.FDwfAnalogOutConfigure(hdwf, channel, c_int(1)).__str__())
